25ea178802d4e0f8558feb9d3f52fff915d85085
franzwa <francois@dnvt.me>
2020-10-02 10:20:44 +0200

    test

--- src/Sections/Guidelines/DeviceSection.js
+++ src/Sections/Guidelines/DeviceSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Device from "../../Components/Devices/Device";
-import browserP from "../../Assets/Images/Guidelines/browser1.png";
-import browserW from "../../Assets/Images/Guidelines/browser1.webp";
+// import browserP from "../../Assets/Images/Guidelines/browser1.png";
+// import browserW from "../../Assets/Images/Guidelines/browser1.webp";
 import SpacerFullHeight from "../../Utils/Spacer/Variations/SpacerFullHeight";
 import Spacer from "../../Utils/Spacer/Spacer";
 import Container from "../../Components/Containers/Container";
@@ -9,7 +9,7 @@ import { useWindowSize } from "../../Hooks/useWindowSize";
 
 export const DeviceSection = () => {
   const window = useWindowSize();
-  const images = [browserP, browserW];
+  // const images = [browserP, browserW];
 
   if (window.width > "992")
     return (
