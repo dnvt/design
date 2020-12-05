a8d12446b5e5d576d1f0a6f03061452b74398dae
franzwa <francois@dnvt.me>
2020-12-05 11:58:45 +0100

    Updates

---
+++ precache-manifest.a0539e4de278ac0b7619d4d2d09d3b34.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150a631dec55717deecb4bde8cf4cbe8",
+    "url": "/design/index.html"
+  },
+  {
+    "revision": "37fb2ac4f7e8f0bf8239",
+    "url": "/design/static/css/main.dbc53280.chunk.css"
+  },
+  {
+    "revision": "9aa92588c59003390dba",
+    "url": "/design/static/js/2.b9a407f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/design/static/js/2.b9a407f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fb2ac4f7e8f0bf8239",
+    "url": "/design/static/js/main.3bbac13d.chunk.js"
+  },
+  {
+    "revision": "78ee108af4e5ddf6feec",
+    "url": "/design/static/js/runtime-main.3f359fd2.js"
+  },
+  {
+    "revision": "8531fa7be7330ba846a2e291d229c77a",
+    "url": "/design/static/media/Airbus.8531fa7b.webp"
+  },
+  {
+    "revision": "a06a6150b60461c43c8c1d0be0c62432",
+    "url": "/design/static/media/Branding.a06a6150.png"
+  },
+  {
+    "revision": "bc06d47089ea7b4d6ad4ba96c2544add",
+    "url": "/design/static/media/Branding.bc06d470.webp"
+  },
+  {
+    "revision": "af72c5c302b0ec5891edf2acd9c78f54",
+    "url": "/design/static/media/CoverHero.af72c5c3.png"
+  },
+  {
+    "revision": "d4f0d021ef27b18416602b810522424a",
+    "url": "/design/static/media/CoverHero.d4f0d021.webp"
+  },
+  {
+    "revision": "28015144f50da5b49dd5e1ec415e6eeb",
+    "url": "/design/static/media/Coverjs.28015144.webp"
+  },
+  {
+    "revision": "615bb02ee3cbced69e454442e7da0199",
+    "url": "/design/static/media/Coverjs.615bb02e.png"
+  },
+  {
+    "revision": "aa717a156f61ca5aa9fc26b73a331300",
+    "url": "/design/static/media/Elastic.aa717a15.webp"
+  },
+  {
+    "revision": "e5e7ad7692aba4ad2dc940548837c563",
+    "url": "/design/static/media/Elastic.e5e7ad76.png"
+  },
+  {
+    "revision": "0f7377ac0c62569fca72604cd0f06fa1",
+    "url": "/design/static/media/GTSectraTrial-Bd.0f7377ac.woff"
+  },
+  {
+    "revision": "272cff082681b439d6d6d7af25b9d15f",
+    "url": "/design/static/media/GTSectraTrial-Bd.272cff08.woff2"
+  },
+  {
+    "revision": "06ce1e81f72e01189b09168963fb4a88",
+    "url": "/design/static/media/GTSectraTrial-Rg.06ce1e81.woff"
+  },
+  {
+    "revision": "19ce817dd80c830137687bd152eae07b",
+    "url": "/design/static/media/GTSectraTrial-Rg.19ce817d.woff2"
+  },
+  {
+    "revision": "6dc4e9fd45c4d5fb6ff11ba91671f5ef",
+    "url": "/design/static/media/Kipfit.6dc4e9fd.png"
+  },
+  {
+    "revision": "f1765a571dc566ee0b7516c18e96d5f4",
+    "url": "/design/static/media/Kipfit.f1765a57.webp"
+  },
+  {
+    "revision": "176c49f15b850dc1a99948f0226c9d83",
+    "url": "/design/static/media/Matrix.176c49f1.webp"
+  },
+  {
+    "revision": "5a8ff615efc94d3cceb1aadf06ec070d",
+    "url": "/design/static/media/Matrix.5a8ff615.png"
+  },
+  {
+    "revision": "27ceadc1090431a3279a9a72f58e71f0",
+    "url": "/design/static/media/Mimi.27ceadc1.png"
+  },
+  {
+    "revision": "33829df450cea561eacdebaf93945a30",
+    "url": "/design/static/media/Mimi.33829df4.webp"
+  },
+  {
+    "revision": "2116d9530cb6f347087a3a83ecd38e34",
+    "url": "/design/static/media/Norse.2116d953.png"
+  },
+  {
+    "revision": "c91da41b519955b65099638191293209",
+    "url": "/design/static/media/Norse.c91da41b.webp"
+  },
+  {
+    "revision": "d87962298b72c8cb8652a061c50877b0",
+    "url": "/design/static/media/Siemens.d8796229.webp"
+  },
+  {
+    "revision": "2986fe05a469804398e6fd78a0553a9e",
+    "url": "/design/static/media/Stars.2986fe05.webp"
+  },
+  {
+    "revision": "f61701a3f2b8ce02a6755b1ff3b663bc",
+    "url": "/design/static/media/Stars.f61701a3.png"
+  },
+  {
+    "revision": "bbc92297b4e403e5fd1b460bc5b2a4d8",
+    "url": "/design/static/media/System.bbc92297.webp"
+  },
+  {
+    "revision": "bd57a52b07390e70ac0948d5afdc16a6",
+    "url": "/design/static/media/System.bd57a52b.png"
+  },
+  {
+    "revision": "af84162a70058614c71cb5c8cf17beb3",
+    "url": "/design/static/media/Telekom.af84162a.webp"
+  },
+  {
+    "revision": "2ec7ab32d14db122fc7d1e65531b691c",
+    "url": "/design/static/media/Waste.2ec7ab32.webp"
+  },
+  {
+    "revision": "576347c63ee41cf4de719b1935bacb32",
+    "url": "/design/static/media/Waste.576347c6.png"
+  },
+  {
+    "revision": "156b50d1aa7d9211561510712c81dde8",
+    "url": "/design/static/media/iAWriterQuattroS-Regular.156b50d1.woff2"
+  },
+  {
+    "revision": "d67923df405e9a661802c86049b35dbb",
+    "url": "/design/static/media/iAWriterQuattroS-Regular.d67923df.woff"
+  },
+  {
+    "revision": "6f94a6bdccea2a36346266a62270a358",
+    "url": "/design/static/media/phone01.6f94a6bd.webp"
+  },
+  {
+    "revision": "b8514efc65394dab0db01d222b80620b",
+    "url": "/design/static/media/phone01.b8514efc.png"
+  },
+  {
+    "revision": "7effc34b824cfd331180a8efb8473e37",
+    "url": "/design/static/media/phone02.7effc34b.png"
+  },
+  {
+    "revision": "e6d89cb4fc7d4939999af7730e8e15e6",
+    "url": "/design/static/media/phone02.e6d89cb4.webp"
+  },
+  {
+    "revision": "37beabec99b24d7002696f9abb5deeaf",
+    "url": "/design/static/media/phone03.37beabec.png"
+  },
+  {
+    "revision": "86e8580a237eb09e8b7b6f96a4521c07",
+    "url": "/design/static/media/phone03.86e8580a.webp"
+  },
+  {
+    "revision": "1aeb73cc10503b2870e08eab4c2e9ae5",
+    "url": "/design/static/media/phone04.1aeb73cc.png"
+  },
+  {
+    "revision": "7c02c38e6333f3d765f42f669ecc6e87",
+    "url": "/design/static/media/phone04.7c02c38e.webp"
+  }
+]);
