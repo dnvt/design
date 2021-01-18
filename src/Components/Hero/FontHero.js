be8ee8139f547b949871422a435d32e7adf66cd4
franzwa <francois@dnvt.me>
2021-01-18 13:12:04 +0100

    updating yarn and npm packages

--- src/Components/Hero/FontHero.js
+++ src/Components/Hero/FontHero.js
@@ -1,22 +1,23 @@
 import React, { memo } from "react";
-// import Font from "../../Utils/Font/Font";
-// import HeroStyle from "./Hero-style";
-import { useWindowSize } from "../../Hooks/useWindowSize";
+import Font from "../../Utils/Font/Font";
+import HeroStyle from "./Hero-style";
+// import { useWindowSize } from "../../Hooks/useWindowSize";
 
-import Hero from "./Hero";
+// import Hero from "./Hero";
 
 const FontHero = (props) => {
-  // const classes = HeroStyle();
-  const window = useWindowSize();
+  const classes = HeroStyle();
+  // const window = useWindowSize();
 
-  if (window.width > 768) {
-    return <Hero value={props.value} bottom={props.bottom} />;
-  }
+  // if (window.width > 768) {
+  //   return <Hero value={props.value} bottom={props.bottom} />;
+  // }
 
-  return null;
-  // <div className={classes.Fontimation} style={{ zIndex: "-1" }}>
-  //   <Font type='hero'>{props.value}</Font>
-  // </div>
+  return (
+    <div className={classes.Fontimation} style={{ zIndex: "-1" }}>
+      <Font type='hero'>{props.value}</Font>
+    </div>
+  );
 };
 
 export default memo(FontHero);
